d8f8c73dc0435654899c0f208d60a8c1410533c1
Adam Calhoun and Jun Li <jun@>
2017-11-27 09:45:01 -0800

    done

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,22 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+
+var result = sub(number1, number2);
 alert(result);
