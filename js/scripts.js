bf04212066564b9994d92ad80e02719f51b49a0d
Jun Li and Sam Gespass <sam@>
2017-11-29 10:19:09 -0800

    add jquery event handlers

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,51 @@
+// Business (or back-end) logic:
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var sub = function(number1, number2) {
+var subtract = function(number1, number2) {
   return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
 };
 
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var mult = function(number1, number2) {
-  return number1 * number2;
-};
+// Everything below this line is user interface (or front-end) logic:
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").prepend(result + "<br>");
+  });
 
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").prepend(result + "<br>");
+  });
 
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output").prepend(result + "<br>");
+  });
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-var result = sub(number1, number2);
-alert(result);
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output").prepend(result + "<br>");
+  });
+});
